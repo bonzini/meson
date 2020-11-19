62a51f4eee6ae04ba864a69ddfd9616b3ee627d9
Paolo Bonzini <pbonzini@redhat.com>
2020-11-19 12:15:13 +0100

    test

--- test cases/common/207 tap tests/tester.c
+++ test cases/common/207 tap tests/tester.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
 int main(int argc, char **argv) {
+    srand(time(NULL) + getpid());
+    int n = rand() % 250 + 5;
+    usleep(n * 100000);
     if (argc != 2) {
         fprintf(stderr, "Incorrect number of arguments, got %i\n", argc);
         return 1;
