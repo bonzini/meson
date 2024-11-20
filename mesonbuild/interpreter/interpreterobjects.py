cf947abc101eb056726053010d9431d772f727c3
Paolo Bonzini <pbonzini@redhat.com>
2024-11-20 12:10:37 +0100

    build, interpreter: associate a doctest target to a BuildTarget

--- mesonbuild/interpreter/interpreterobjects.py
+++ mesonbuild/interpreter/interpreterobjects.py
@@ -809,6 +809,11 @@ class Test(MesonInterpreterObject):
     def get_name(self) -> str:
         return self.name
 
+
+class Doctest(Test):
+    target: BuildTarget
+
+
 class NullSubprojectInterpreter(HoldableObject):
     pass
 
