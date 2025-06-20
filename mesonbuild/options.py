bonzini/meson

commit 8dcc9d3423762f9cf996af87b8a2c71c7111aa32

options: accept compiler and built-in options in --reconfigure and "meson configure"
Follow the same logic that is used at the end of the first invocation.
This fixes

   meson setup --reconfigure -Db_ndebug=true

on a project that has no language that defines b_ndebug.

Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/options.py
@@ -1059,8 +1059,13 @@ class OptionStore:
     def set_option_maybe_root(self, o: OptionKey, new_value: str) -> bool:
         if o in self.options:
             return self.set_option(o, new_value)
-        o = o.as_root()
-        return self.set_option(o, new_value)
+        if self.accept_as_pending_option(o):
+            old_value = self.pending_options.get(o, None)
+            self.pending_options[o] = new_value
+            return old_value is None or str(old_value) == new_value
+        else:
+            o = o.as_root()
+            return self.set_option(o, new_value)
 
     def set_from_configure_command(self, D_args: T.List[str], U_args: T.List[str]) -> bool:
         dirty = False

unittests/optiontests.py
@@ -208,6 +208,10 @@ class OptionTests(unittest.TestCase):
         assert optstore.accept_as_pending_option(OptionKey('b_ndebug'))
         assert not optstore.accept_as_pending_option(OptionKey('b_whatever'))
 
+    def test_reconfigure_b_nonexistent(self):
+        optstore = OptionStore(False)
+        optstore.set_from_configure_command(['b_ndebug=true'], [])
+
     def test_subproject_nonexistent(self):
         optstore = OptionStore(False)
         subprojects = {'found'}

commit 998ec8f5b970ce67849a2dcfa8550a70ea1f6f7b

options: fix direction of result
Fixes: 8dcc9d342 ("options: accept compiler and built-in options in --reconfigure and "meson configure"", 2025-05-21
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/options.py
@@ -1085,7 +1085,7 @@ class OptionStore:
         if self.accept_as_pending_option(o, first_invocation=first_invocation):
             old_value = self.pending_options.get(o, None)
             self.pending_options[o] = new_value
-            return old_value is None or str(old_value) == new_value
+            return old_value is None or str(old_value) != new_value
         else:
             o = o.as_root()
             return self.set_option(o, new_value, first_invocation)