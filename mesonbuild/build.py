ba170aa98246d752a9fcf2d2590e31ae3b463338
Xavier Claessens <xclaessens@netflix.com>
2025-10-14 14:14:40 -0400

    Allow overriding a program with a CustomTarget
    Move implementation to a function read to become public API.

--- mesonbuild/build.py
+++ mesonbuild/build.py
@@ -3335,9 +3335,6 @@ class LocalProgram(programs.Program):
                  depends: T.Optional[T.List[T.Union[BuildTarget, CustomTarget]]] = None,
                  depend_files: T.Optional[T.List[File]] = None) -> None:
         super().__init__()
-        if isinstance(program, CustomTarget):
-            if len(program.outputs) != 1:
-                raise InvalidArguments('CustomTarget used as LocalProgram must have exactly one output.')
         self.name = program.name
         self.for_machine = program.for_machine
         self.program = program
