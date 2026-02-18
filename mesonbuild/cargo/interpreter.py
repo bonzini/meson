bonzini/meson

commit f864690f3a74ed4f751e9b6d9a8855ec16030c65

cargo: move fetching to _dep_package
This allows modifying the version number before the package is fetched.

Extracted from a patch by Xavier Claessens <xclaessens@netflix.com>.

Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/cargo/interpreter.py
@@ -120,13 +120,22 @@ class Interpreter:
         if pkg:
             return pkg, True
         meson_depname = _dependency_name(package_name, api)
-        subdir, _ = self.environment.wrap_resolver.resolve(meson_depname)
+        return self._fetch_package_from_subproject(package_name, meson_depname)
+
+    def _fetch_package_from_subproject(self, package_name: str, meson_depname: str) -> T.Tuple[PackageState, bool]:
+        subp_name, _ = self.environment.wrap_resolver.find_dep_provider(meson_depname)
+        subdir, _ = self.environment.wrap_resolver.resolve(subp_name)
         subprojects_dir = os.path.join(subdir, 'subprojects')
         self.environment.wrap_resolver.load_and_merge(subprojects_dir, T.cast('SubProject', meson_depname))
         manifest = self._load_manifest(subdir)
         downloaded = \
-            meson_depname in self.environment.wrap_resolver.wraps and \
-            self.environment.wrap_resolver.wraps[meson_depname].type is not None
+            subp_name in self.environment.wrap_resolver.wraps and \
+            self.environment.wrap_resolver.wraps[subp_name].type is not None
+        key = PackageKey(package_name, version.api(manifest.package.version))
+
+        pkg = self.packages.get(key)
+        if pkg:
+            return pkg, True
         pkg = PackageState(manifest, downloaded)
         self.packages[key] = pkg
         # Merge target specific dependencies that are enabled
@@ -141,7 +150,12 @@ class Interpreter:
         return pkg, False
 
     def _dep_package(self, dep: Dependency) -> PackageState:
-        return self.packages[PackageKey(dep.package, dep.api)]
+        if dep.git:
+            _, _, directory = _parse_git_url(dep.git, dep.branch)
+            dep_pkg, _ = self._fetch_package_from_subproject(dep.package, directory)
+        else:
+            dep_pkg, _ = self._fetch_package(dep.package, dep.api)
+        return dep_pkg
 
     def _load_manifest(self, subdir: str) -> Manifest:
         manifest_ = self.manifests.get(subdir)
@@ -165,7 +179,7 @@ class Interpreter:
             # It could be build/dev/target dependency. Just ignore it.
             return
         pkg.required_deps.add(depname)
-        dep_pkg, _ = self._fetch_package(dep.package, dep.api)
+        dep_pkg = self._dep_package(dep)
         if dep.default_features:
             self._enable_feature(dep_pkg, 'default')
         for f in dep.features:

commit 29be1cb5870f055a880aaae55912dbc93ab1d77d

cargo: correct subproject name when git crates are requested
When workspace.subproject() looks up a package, it is currently using
always the NAME-API-rs convention, but that is incorrect for git-method
subprojects (because you don't want the name to change whenever the
API is changed in the git repository).

For external packages, use the subproject name as passed to
_fetch_package_from_subproject and save it for later use in
workspace.subproject().

Fixes: f864690f3 ("cargo: move fetching to _dep_package", 2025-10-14)
Reported-by: Alexander Nutz <alexander.nutz@vxcc.dev>
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/cargo/interpreter.py
@@ -104,6 +104,10 @@ class PackageState:
     ws_member: T.Optional[str] = None
     # Package configuration state
     cfg: T.Optional[PackageConfiguration] = None
+    # Subproject name as known to the wrap resolver (may differ from the
+    # meson dep name for git sources, where the wrap is named after the
+    # git directory rather than the crate name + api version).
+    subproject_name: T.Optional[str] = None
 
     def get_env_dict(self, environment: Environment, subdir: str) -> T.Dict[str, str]:
         """Get environment variables for this package."""
@@ -199,6 +203,8 @@ class PackageState:
         return abis
 
     def get_subproject_name(self) -> SubProject:
+        if self.subproject_name is not None:
+            return SubProject(self.subproject_name)
         dep = _dependency_name(self.manifest.package.name, self.manifest.package.api)
         return SubProject(dep)
 
@@ -493,6 +499,7 @@ class Interpreter:
         ws = self._get_workspace(manifest, subdir, downloaded=downloaded)
         member = ws.packages_to_member[package_name]
         pkg = self._require_workspace_member(ws, member)
+        pkg.subproject_name = subp_name
         return pkg
 
     def _prepare_package(self, pkg: PackageState) -> None: