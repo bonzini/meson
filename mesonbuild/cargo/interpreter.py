bonzini/meson

commit f864690f3a74ed4f751e9b6d9a8855ec16030c65

cargo: move fetching to _dep_package
This allows modifying the version number before the package is fetched.

Extracted from a patch by Xavier Claessens <xclaessens@netflix.com>.

Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/cargo/interpreter.py
@@ -120,13 +120,22 @@ class Interpreter:
         if pkg:
             return pkg, True
         meson_depname = _dependency_name(package_name, api)
-        subdir, _ = self.environment.wrap_resolver.resolve(meson_depname)
+        return self._fetch_package_from_subproject(package_name, meson_depname)
+
+    def _fetch_package_from_subproject(self, package_name: str, meson_depname: str) -> T.Tuple[PackageState, bool]:
+        subp_name, _ = self.environment.wrap_resolver.find_dep_provider(meson_depname)
+        subdir, _ = self.environment.wrap_resolver.resolve(subp_name)
         subprojects_dir = os.path.join(subdir, 'subprojects')
         self.environment.wrap_resolver.load_and_merge(subprojects_dir, T.cast('SubProject', meson_depname))
         manifest = self._load_manifest(subdir)
         downloaded = \
-            meson_depname in self.environment.wrap_resolver.wraps and \
-            self.environment.wrap_resolver.wraps[meson_depname].type is not None
+            subp_name in self.environment.wrap_resolver.wraps and \
+            self.environment.wrap_resolver.wraps[subp_name].type is not None
+        key = PackageKey(package_name, version.api(manifest.package.version))
+
+        pkg = self.packages.get(key)
+        if pkg:
+            return pkg, True
         pkg = PackageState(manifest, downloaded)
         self.packages[key] = pkg
         # Merge target specific dependencies that are enabled
@@ -141,7 +150,12 @@ class Interpreter:
         return pkg, False
 
     def _dep_package(self, dep: Dependency) -> PackageState:
-        return self.packages[PackageKey(dep.package, dep.api)]
+        if dep.git:
+            _, _, directory = _parse_git_url(dep.git, dep.branch)
+            dep_pkg, _ = self._fetch_package_from_subproject(dep.package, directory)
+        else:
+            dep_pkg, _ = self._fetch_package(dep.package, dep.api)
+        return dep_pkg
 
     def _load_manifest(self, subdir: str) -> Manifest:
         manifest_ = self.manifests.get(subdir)
@@ -165,7 +179,7 @@ class Interpreter:
             # It could be build/dev/target dependency. Just ignore it.
             return
         pkg.required_deps.add(depname)
-        dep_pkg, _ = self._fetch_package(dep.package, dep.api)
+        dep_pkg = self._dep_package(dep)
         if dep.default_features:
             self._enable_feature(dep_pkg, 'default')
         for f in dep.features:

commit b5579eb52af7b3b63fcc65a7ecb6ac6b7ed2f7b6

cargo: correct subproject name for crates with git source
Use the subproject name from _parse_git_url directly, instead of
treating it as a meson dependency name (which it is not).

Note that while the change affects the "elif dep.git:" branch, that
branch is not visible in the patch because it *keeps calling
_fetch_package_from_subproject* rather than the newly-split
_fetch_package_from_provider.

While at it, move the meson_depname variable out of
_fetch_package, since the caller does not need it.

Fixes: f864690f3 ("cargo: move fetching to _dep_package", 2025-10-14)
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/cargo/interpreter.py
@@ -439,8 +439,7 @@ class Interpreter:
         pkg = self.packages.get(key)
         if pkg:
             return pkg
-        meson_depname = _dependency_name(package_name, api)
-        return self._fetch_package_from_subproject(package_name, meson_depname)
+        return self._fetch_package_from_provider(package_name, api)
 
     def _resolve_package(self, package_name: str, version_constraints: T.List[str]) -> T.Optional[CargoLockPackage]:
         """From all available versions from Cargo.lock, pick the most recent
@@ -464,7 +463,8 @@ class Interpreter:
         api = version.api(cargo_pkg.version)
         return self._fetch_package(package_name, api)
 
-    def _fetch_package_from_subproject(self, package_name: str, meson_depname: str) -> PackageState:
+    def _fetch_package_from_provider(self, package_name: str, api: str) -> PackageState:
+        meson_depname = _dependency_name(package_name, api)
         subp_name, _ = self.environment.wrap_resolver.find_dep_provider(meson_depname)
         if subp_name is None:
             if self.cargolock is None:
@@ -479,9 +479,12 @@ class Interpreter:
                 similar_msg = 'Cargo.lock does not contain this crate name.'
             raise MesonException(f'Dependency {meson_depname!r} not found in any wrap files or Cargo.lock; {similar_msg} This could be a Meson bug, please report it.')
 
+        return self._fetch_package_from_subproject(package_name, subp_name)
+
+    def _fetch_package_from_subproject(self, package_name: str, subp_name: str) -> PackageState:
         subdir, _ = self.environment.wrap_resolver.resolve(subp_name)
         subprojects_dir = os.path.join(subdir, 'subprojects')
-        self.environment.wrap_resolver.load_and_merge(subprojects_dir, SubProject(meson_depname))
+        self.environment.wrap_resolver.load_and_merge(subprojects_dir, SubProject(subp_name))
         manifest, _ = self._load_manifest(subdir)
         downloaded = \
             subp_name in self.environment.wrap_resolver.wraps and \